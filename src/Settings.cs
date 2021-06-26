e1b4be33602e7ee5d37ab99070318a4751bacc81
Milovidov Vasya <milovidov.vasya@gmail.com>
2021-06-26 20:53:24 +0300

    rm unuse var

--- src/Settings.cs
+++ src/Settings.cs
@@ -5,11 +5,6 @@ using System.Threading.Tasks;
 
 namespace WebApi {
 	public class Settings {
-		public UserNameRuleSettings UserNameRule { get; set; }
 
-		public class UserNameRuleSettings {
-			public int MaxLength { get; set; }
-			public string ValidCharsRegexp { get; set; }
-		}
 	}
 }
